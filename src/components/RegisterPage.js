a1cac59ce46354ca223a53cd4110757ec3f6b9b9
oussama teffal <105069140+oussama1099@users.noreply.github.com>
2023-05-29 03:53:17 +0100

    new commit

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 import background from '../images/background.jpg';
+import axios from 'axios';
 
 const Container = styled.div`
   display: flex;
@@ -27,7 +28,7 @@ const Form = styled.form`
 `;
 
 const Label = styled.label`
-  font-size: 18px;
+  font-size: 100%;
   color: rgb(255,255,255,90%);
   margin-bottom: 2%;
   margin-left: 8%;
@@ -37,7 +38,7 @@ const Label = styled.label`
 `;
 
 const Input = styled.input`
-  margin-bottom: 20px;
+  margin-bottom: 3%;
   padding: 10px;
   border-radius: 5px;
   border: 1px solid #cccccc;
@@ -48,7 +49,7 @@ const Input = styled.input`
 
 const Select = styled.select`
   margin-bottom: 10%;
-  padding: 10px;
+  padding: 2%;
   border-radius: 5px;
   border: 1px solid #cccccc;
   margin-left: 5%;
@@ -57,7 +58,7 @@ const Select = styled.select`
 `;
 
 const Button = styled.button`
-  padding: 10px;
+  padding: 3%;
   border-radius: 5px;
   border: none;
   background-color: rgb(0, 185, 169);
@@ -69,6 +70,7 @@ const Button = styled.button`
 `;
 
 const Title = styled.h1`
+  font-size:200%;
   font-weight: 600;
   font-family: 'Raleway';
   color: white;
@@ -86,109 +88,89 @@ const ErrorMessage = styled.p`
   font-style: italic;
 `;
 
-const FormPage1 = ({ form, setForm }) => {
-    return (
-        <Form>
-            <Title>INSCRIPTION</Title>
-            <Label>Nom</Label>
-            <Input
-                type="text"
-                placeholder="Nom"
-                required
-                value={form.name}
-                onChange={(e) => setForm({ ...form, name: e.target.value })}
-            />
-            <Label>Email</Label>
-            <Input
-                type="email"
-                placeholder="Email"
-                required
-                value={form.email}
-                onChange={(e) => setForm({ ...form, email: e.target.value })}
-            />
-            <Label>Profession</Label>
-            <Select
-                required
-                value={form.profession}
-                onChange={(e) => setForm({ ...form, profession: e.target.value })}
-            >
-                <option value="Laureat">Lauréat</option>
-                <option value="Etudiant">Etudiant</option>
-                <option value="Professeur">Professeur</option>
-            </Select>
-            <Button onClick={() => setForm({ ...form, currentPage: 2 })}>Suivant</Button>
-        </Form>
-    );
-};
-
-const FormPage2 = ({ form, setForm }) => {
+const RegisterPage = () => {
+    const [form, setForm] = useState({
+        nom: '',
+        prenom: '',
+        email: '',
+        password: '',
+        statut: '',
+    });
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Remplacer cette vérification par votre propre logique de validation
-        if (form.currentPage === 2) {
-            // Si nous sommes à la dernière page du formulaire, nous soumettons le formulaire
-            // et redirigeons l'utilisateur
-            console.log(form); // Remplacer par votre propre logique d'inscription
-            navigate("/home");
-        } else {
-            // Sinon, nous passons à la page suivante du formulaire
-            setForm({ ...form, currentPage: form.currentPage + 1 });
-        }
+        // Création d'un objet contenant les données du formulaire
+        const formData = {
+            nom: form.nom,
+            prenom: form.prenom,
+            email: form.email,
+            password: form.password,
+            statut: form.statut,
+        };
+
+        // Envoi des données à l'API backend en utilisant Axios
+        axios.post("https://projettutor.herokuapp.com/register", formData)
+            .then(response => {
+                // Si la réponse de l'API est réussie, redirige l'utilisateur vers la page d'accueil
+                navigate("/home");
+            })
+            .catch(error => {
+                // Gérer les erreurs de connexion ou autres erreurs
+                console.log("Une erreur s'est produite lors de l'envoi des données.", error);
+            });
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Title>INSCRIPTION</Title>
-            <Label>Entreprise</Label>
-            <Input
-                type="text"
-                placeholder="Entreprise"
-                required
-            />
-            <Label>Email Entreprise</Label>
-            <Input
-                type="email"
-                placeholder="Email entreprise"
-                required
-
-            />
-            <Label>Site web</Label>
-            <Input
-                type="text"
-                placeholder="Siteweb"
-                required
-
-            />
-            <Label>Domaine</Label>
-            <Input
-                type="domaine"
-                placeholder="domaine"
-                required
-
-            />
-            <Button type="submit">Envoyer</Button> {/* Changement du texte du bouton */}
-        </Form>
-    );
-};
-
-const RegisterPage = () => {
-    const [form, setForm] = useState({
-        currentPage: 1,
-        name: '',
-        email: '',
-        profession: '',
-        // Ajoutez d'autres champs au besoin
-    });
-    const navigate = useNavigate();
-
-    return (
         <Container>
-            {form.currentPage === 1 && <FormPage1 form={form} setForm={setForm} />}
-            {form.currentPage === 2 && <FormPage2 form={form} setForm={setForm} />}
-            {/* Ajoutez plus de pages de formulaire au besoin */}
+            <Form onSubmit={handleSubmit}>
+                <Title>INSCRIPTION</Title>
+                <Label>Nom</Label>
+                <Input
+                    type="text"
+                    placeholder="Nom"
+                    required
+                    value={form.nom}
+                    onChange={(e) => setForm({ ...form, nom: e.target.value })}
+                />
+                <Label>Prénom</Label>
+                <Input
+                    type="text"
+                    placeholder="Prénom"
+                    required
+                    value={form.prenom}
+                    onChange={(e) => setForm({ ...form, prenom: e.target.value })}
+                />
+                <Label>Email</Label>
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={form.email}
+                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                />
+                <Label>Password</Label>
+                <Input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={form.password}
+                    onChange={(e) => setForm({ ...form, password: e.target.value })}
+                />
+                <Label>Statut</Label>
+                <Select
+                    required
+                    value={form.statut}
+                    onChange={(e) => setForm({ ...form, statut: e.target.value })}
+                >
+                    <option value="">Choisir un statut</option>
+                    <option value="Laureat">Lauréat</option>
+                    <option value="Etudiant">Etudiant</option>
+                    <option value="Professeur">Professeur</option>
+                </Select>
+                <Button type="submit">Envoyer</Button>
+            </Form>
         </Container>
     );
 };
